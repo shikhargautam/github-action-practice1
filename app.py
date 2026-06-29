a9666b2ac910e0c0eb33c1475a6982300a8fe751
shikhargautam <39986666+shikhargautam@users.noreply.github.com>
2026-06-29 14:02:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
+#making this comment to test linter
 @app.route('/')
 def hello_world():
     return render_template('index.html')
